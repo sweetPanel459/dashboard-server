01a3febc07f49d2421b9ae40bc6d19a634337549
otpJs <sssantiago459@gmail.com>
2024-09-25 17:03:59 -0500

    feat: Creating controllers for the dashboard

--- src/controller/dashboard-controller.js
+++ src/controller/dashboard-controller.js
@@ -1,10 +1,18 @@
-const get_table = (req, res) => { };
+const get_table = (req, res) => {
+  console.log(req, res);
+};
 
-const update_table = (req, res) => { };
+const update_table = (req, res) => {
+  console.log(req, res);
+};
 
-const upload_table = (req, res) => { };
+const upload_table = (req, res) => {
+  console.log(req, res);
+};
 
-const delete_table = (req, res) => { };
+const delete_table = (req, res) => {
+  console.log(req, res);
+};
 
 export const dashboard_controller = {
   get_table,

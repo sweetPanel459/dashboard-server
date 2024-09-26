2af9db80e3f7be2f7382595a58eb05a0a1571e03
otpJs <sssantiago459@gmail.com>
2024-09-25 21:31:14 -0500

    feat: I added a new route to delete a specific row

--- src/controller/dashboard-controller.js
+++ src/controller/dashboard-controller.js
@@ -14,9 +14,14 @@ const delete_table = (req, res) => {
   console.log(req, res);
 };
 
+const delete_row_table = (req, res) => {
+  console.log(req, res);
+};
+
 export const dashboard_controller = {
   get_table,
   update_table,
   upload_table,
   delete_table,
+  delete_row_table,
 };

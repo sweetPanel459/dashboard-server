1804c51a67d86d1f02fcbbf11ee313e2051830f8
otpJs <sssantiago459@gmail.com>
2024-09-26 23:50:51 -0500

    feat: created admin auth controller

--- src/controller/admin-auth-controller.js
+++ src/controller/admin-auth-controller.js
@@ -1,5 +1,22 @@
-const get_admin_data = (req, res) => console.log("hoal desde el get");
+import { auth_admin_model } from "../models/auth-admin-model.js";
 
-const register_admin = (req, res) => console.log("hola desde register adimin");
+import { error_handler } from "../helpers/error-handler.js";
+import { response_handler } from "../helpers/response-handler.js";
+
+const get_admin_data = (req, res) => {
+  try {
+    auth_admin_model.get_admin_data_model(req, response_handler(data, err));
+  } catch (error) {
+    error_handler(error, res);
+  }
+};
+
+const register_admin = (req, res) => {
+  try {
+    auth_admin_model.register_admin_model(req, response_handler(data, err));
+  } catch (error) {
+    error_handler(error, res);
+  }
+};
 
 export const auth_controller = { get_admin_data, register_admin };

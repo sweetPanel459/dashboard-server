36b4752e33c48b35c73bdb37f45165bd81f7fa02
otpJs <sssantiago459@gmail.com>
2024-09-27 20:47:41 -0500

    feat: created execute query function

--- src/helpers/custom-query-sql.js
+++ src/helpers/custom-query-sql.js
@@ -6,3 +6,10 @@ export const custom_sql_query = async (query, params) => {
 
   return query_response;
 };
+
+export const custm_sql_execute = async (query, params) => {
+  const [rows, fileds] = await pool.execute(query, [...params]);
+  const query_response = { rows, fields };
+
+  return query_response;
+};

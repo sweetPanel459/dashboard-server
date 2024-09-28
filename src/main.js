8636bc6117daddd28fc448d8b84c14bb31e64413
otpJs <sssantiago459@gmail.com>
2024-09-28 15:57:23 -0500

    fix: change order lines

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ app.set("case sensitive routing", true);
 app.set("env", "development");
 app.set("port", port);
 
-app.use(router);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
 const PORT = app.get("port");
 app.listen(PORT, () => console.log(`servidor abierto en el puerto ${PORT}`));
